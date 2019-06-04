36f39be6ba048a63bf29234e5cbaea7d21378369
o.sharafislamova <2260662@mail.ru>
2019-06-04 11:39:32 +0300

    refactoring test case

--- test_add_group.py
+++ test_add_group.py
@@ -14,29 +14,49 @@ class UntitledTestCase(unittest.TestCase):
 
     def test_untitled_test_case(self):
         wb = self.wb
-        wb.get("http://localhost/addressbook/")
-        wb.find_element_by_name("user").click()
-        wb.find_element_by_name("user").clear()
-        wb.find_element_by_name("user").send_keys("admin")
-        wb.find_element_by_name("pass").clear()
-        wb.find_element_by_name("pass").send_keys("secret")
-        wb.find_element_by_xpath("//input[@value='Login']").click()
-        wb.find_element_by_link_text("groups").click()
+        self.open_home_page(wb)
+        self.login(wb, username="admin", password="secret")
+        self.open_group_page(wb)
+        self.create_group(wb, name="name", header="sdasd", footer="asd")
+        self.return_to_groups_page(wb)
+        self.logout(wb)
+
+    def logout(self, wb):
+        wb.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wb):
+        wb.find_element_by_link_text("group page").click()
+
+    def create_group(self, wb, name, header, footer):
+        # init group creation
         wb.find_element_by_xpath("(//input[@name='new'])[2]").click()
+        # fill group firm
         wb.find_element_by_name("group_name").click()
         wb.find_element_by_name("group_name").clear()
-        wb.find_element_by_name("group_name").send_keys("name")
+        wb.find_element_by_name("group_name").send_keys(name)
         wb.find_element_by_name("group_header").click()
         wb.find_element_by_name("group_header").clear()
-        wb.find_element_by_name("group_header").send_keys("11111")
+        wb.find_element_by_name("group_header").send_keys(header)
         wb.find_element_by_name("group_footer").click()
         wb.find_element_by_name("group_footer").clear()
-        wb.find_element_by_name("group_footer").send_keys("11111")
+        wb.find_element_by_name("group_footer").send_keys(footer)
+        # submit group creation
         wb.find_element_by_name("submit").click()
-        wb.find_element_by_link_text("group page").click()
-        wb.find_element_by_link_text("home").click()
-        wb.find_element_by_link_text("Logout").click()
-    
+
+    def open_group_page(self, wb):
+        wb.find_element_by_link_text("groups").click()
+
+    def login(self, wb, username, password):
+        wb.find_element_by_name("user").click()
+        wb.find_element_by_name("user").clear()
+        wb.find_element_by_name("user").send_keys(username)
+        wb.find_element_by_name("pass").clear()
+        wb.find_element_by_name("pass").send_keys(password)
+        wb.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wb):
+        wb.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wb.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
