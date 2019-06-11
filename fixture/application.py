c700b1c19e220f4c4aeb4a13529bd085a4b5d925
o.sharafislamova <2260662@mail.ru>
2019-06-11 16:04:09 +0300

    выделили вспомогательный класс SessionHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,15 +1,12 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        self.session = SessionHelper(self)
 
     def return_to_groups_page(self):
         wb = self.wd
@@ -37,16 +34,6 @@ class Application:
     def open_group_page(self):
         wb = self.wd
         wb.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wb = self.wd
-        self.open_home_page()
-        wb.find_element_by_name("user").click()
-        wb.find_element_by_name("user").clear()
-        wb.find_element_by_name("user").send_keys(username)
-        wb.find_element_by_name("pass").clear()
-        wb.find_element_by_name("pass").send_keys(password)
-        wb.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self):
         wb = self.wd
---
+++ fixture/session.py
@@ -0,0 +1,19 @@
+
+class SessionHelper:
+
+    def __init__ (self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wb = self.app.wd
+        self.app.open_home_page()
+        wb.find_element_by_name("user").click()
+        wb.find_element_by_name("user").clear()
+        wb.find_element_by_name("user").send_keys(username)
+        wb.find_element_by_name("pass").clear()
+        wb.find_element_by_name("pass").send_keys(password)
+        wb.find_element_by_xpath("//input[@value='Login']").click()
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
