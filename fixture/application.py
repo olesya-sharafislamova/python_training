ee276a8462b1cb31ee5af07a828a8c4f9679bd9d
o.sharafislamova <2260662@mail.ru>
2019-06-11 16:12:18 +0300

    выделили вспомогательный класс GroupHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
 
 class Application:
 
@@ -7,33 +9,7 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-
-    def return_to_groups_page(self):
-        wb = self.wd
-        wb.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wb = self.wd
-        self.open_group_page()
-        # init group creation
-        wb.find_element_by_xpath("(//input[@name='new'])[2]").click()
-        # fill group firm
-        wb.find_element_by_name("group_name").click()
-        wb.find_element_by_name("group_name").clear()
-        wb.find_element_by_name("group_name").send_keys(group.name)
-        wb.find_element_by_name("group_header").click()
-        wb.find_element_by_name("group_header").clear()
-        wb.find_element_by_name("group_header").send_keys(group.header)
-        wb.find_element_by_name("group_footer").click()
-        wb.find_element_by_name("group_footer").clear()
-        wb.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wb.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_group_page(self):
-        wb = self.wd
-        wb.find_element_by_link_text("groups").click()
+        self.group = GroupHelper (self)
 
     def open_home_page(self):
         wb = self.wd
