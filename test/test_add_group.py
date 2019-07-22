866d36b44579cd83b3882a833e14ff36e9cedee3
o.sharafislamova <2260662@mail.ru>
2019-07-22 09:37:31 +0300

    исправила генерацию групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,11 +10,8 @@ def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [
-    Group(name=name, header=header, footer=footer)
-    for name in ["", random_string("name", 10)]
-    for header in ["", random_string("sdasd", 20)]
-    for footer in ["", random_string("asd", 20)]
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
 ]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
