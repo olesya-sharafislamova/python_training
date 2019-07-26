69739adacd5b9c91fe15a9837b4f83d6d5f0570e
o.sharafislamova <2260662@mail.ru>
2019-07-26 09:42:09 +0300

    19

---
+++ data/contacts.py
@@ -0,0 +1,5 @@
+from model.contact import Contact
+
+testdata = [Contact(firstname="name1", middlename="middlename1", lastname="lastname1"),
+           Contact(firstname="name2", middlename="middlename2", lastname="lastname2")]
+
