36caeaadf715620510feca3f868bc7c6e5076c00
o.sharafislamova <2260662@mail.ru>
2019-07-29 16:53:19 +0300

    отношения между объектами

--- fixture/group.py
+++ fixture/group.py
@@ -93,5 +93,17 @@ class GroupHelper:
                 self.group_cache.append(Group(name = text, id = id))
         return list(self.group_cache)
 
+    def delete_group_by_id(self,id):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
 
